935b813f3fd5baa1094f5e93f00bb8dfcbc872b4
mhmmdhakimi <mhmmdhakimi0205@gmail.com>
2026-05-26 16:42:43 +0800

    update

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\mhmmd\develop\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Flutter Projects\smart-health-kiosk"
-export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=C:\Flutter Projects\smart-health-kiosk\macos\Flutter\ephemeral\Packages\.packages\FlutterFramework"
+export "FLUTTER_ROOT=C:\Flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Flutter\Flutter Projects\smart-health-kiosk"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=C:\Flutter\Flutter Projects\smart-health-kiosk\macos\Flutter\ephemeral\Packages\.packages\FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"

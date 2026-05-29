6f0e5bd087d9245f362e69ec67c8131b9f2beb8e
mhmmdhakimi <mhmmdhakimi0205@gmail.com>
2026-05-29 17:54:44 +0800

    update

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -1,11 +1,18 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConfig {
-  static String get firebaseApiKey => dotenv.env['FIREBASE_API_KEY']!;
-  static String get firebaseAppId => dotenv.env['FIREBASE_APP_ID']!;
-  static String get firebaseAppIdWeb => dotenv.env['FIREBASE_APP_ID_WEB'] ?? dotenv.env['FIREBASE_APP_ID']!;
-  static String get emailJsPublicKey => dotenv.env['EMAILJS_PUBLIC_KEY']!;
-  static String get emailJsServiceId => dotenv.env['EMAILJS_SERVICE_ID']!;
-  static String get emailJsTemplateId1 => dotenv.env['EMAILJS_TEMPLATE_ID1']!;
-  static String get emailJsTemplateId2 => dotenv.env['EMAILJS_TEMPLATE_ID2']!;
+  static String _getRequired(String key) {
+    return dotenv.env[key] ??
+        (throw Exception(
+            'CRITICAL CONFIG ERROR: "$key" is missing from your root .env file. Please check your environment variables before booting the app.'));
+  }
+
+  static String get firebaseApiKey => _getRequired('FIREBASE_API_KEY');
+  static String get firebaseAppId => _getRequired('FIREBASE_APP_ID');
+  static String get firebaseAppIdWeb =>
+      dotenv.env['FIREBASE_APP_ID_WEB'] ?? _getRequired('FIREBASE_APP_ID');
+  static String get emailJsPublicKey => _getRequired('EMAILJS_PUBLIC_KEY');
+  static String get emailJsServiceId => _getRequired('EMAILJS_SERVICE_ID');
+  static String get emailJsTemplateId1 => _getRequired('EMAILJS_TEMPLATE_ID1');
+  static String get emailJsTemplateId2 => _getRequired('EMAILJS_TEMPLATE_ID2');
 }
